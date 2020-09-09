158bcb382e36b805c36746280444b392d40a881f
devillkaow <devillkaow@gmail.com>
2020-09-09 22:33:47 +0700

    add route

--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,8 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+Route.group(() => {
+  Route.resource('/profiles', 'ProfileController')
+  
+}).prefix('api/v1')
