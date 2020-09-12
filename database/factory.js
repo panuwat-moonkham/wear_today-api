f798c6eea1cea89b1ec4a27031f4a85240e6142e
panuwat-moonkham <panuwat.moonkham00@gmail.com>
2020-09-13 00:19:04 +0700

    add DatabaseSeeder and factory

--- database/factory.js
+++ database/factory.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const Hash = use('Hash')
+
+
 /*
 |--------------------------------------------------------------------------
 | Factory
@@ -12,10 +15,52 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/User', (faker) => {
+  return {
+    first_name: faker.first(),
+    last_name: faker.last(),
+    user_name: faker.first(),
+    email: faker.email(),
+    password: await Hash.make(faker.password({length: 8}))
+  }
+})
+
+Factory.blueprint('App/Models/Photo', (faker) => {
+  return {
+    category_image: faker.word({ syllables: 3 }),
+    shirt_image: faker.word({ syllables: 3 }),
+    pants_image: faker.word({ syllables: 3 }),
+    shoes_image: faker.word({ syllables: 3 }),
+    jacket_image: faker.word({ syllables: 3 }),
+    hat_image: faker.word({ syllables: 3 }),
+    accessories_image: faker.word({ syllables: 3 })
+  }
+})
+
+Factory.blueprint('App/Models/Comment', (faker) => {
+  return {
+    comment_content: faker.sentence({ words: 10 })
+  }
+})
+
+Factory.blueprint('App/Models/Category', (faker) => {
+    return {
+      category_name: faker.sentence({ words: 2 }),
+      category_detail: faker.sentence({ words: 2 }),
+      shirt_detail: faker.sentence({ words: 2 }),
+      pants_detail: faker.sentence({ words: 2 }),
+      shoes_detail: faker.sentence({ words: 2 }),
+      jacket_detail: faker.sentence({ words: 2 }),
+      hat_detail: faker.sentence({ words: 2 }),
+      accessories_detail: faker.sentence({ words: 2 })
+    }
+  })
+
+  Factory.blueprint('App/Models/Post', (faker) => {
+    return {
+      post_title: faker.sentence({ words: 2 }),
+      description: faker.sentence({ words: 4 })
+    }
+  })
