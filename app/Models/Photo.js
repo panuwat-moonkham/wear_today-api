81dd38574b015c982419b2c1701449783015600a
panuwat-moonkham <panuwat.moonkham00@gmail.com>
2020-09-12 16:44:15 +0700

    add Model photo

---
+++ app/Models/Photo.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Photo extends Model {
+    static get primaryKey(){
+        return 'photo_id'
+    }
+    static get createdAtColumn(){
+        return null;
+    }
+
+    static get updatedAtColumn(){
+        return null;
+    }
+    category(){
+        return this.hasMany('App/Models/Category')
+    }
+}
+
+module.exports = Photo
