c729f78e783bd995870f32bad185d4260130ec64
devillkaow <devillkaow@gmail.com>
2020-09-15 06:38:54 +0700

    edit test-user

--- test/unit/user-validator.spec.js
+++ test/unit/user-validator.spec.js
@@ -3,21 +3,13 @@
 const { test } = use('Test/Suite')('User Validator')
 const userValidator = require('../../service/UserValidator')
 
-test('should receive object as first parameter', async ({ assert }) => {
+test('should return error when pass incorrect data', async ({ assert }) => {
   const validatedData = await userValidator({
     first_name: "John",
     last_name: "Doe",
     username: "adgdg",
-    email: "wrong email",
+    email: "wrong email"
   })
-  assert.isOk(validatedData)
-
-  const validatedData2 = await userValidator("John", "Doe","adgdg", "wrong email", "pass")
-  assert.isNotOk(validatedData2)
-})
-
-test('should return error when pass incorrect data', async ({ assert }) => {
-  const validatedData = await userValidator("John", "Doe","adgdg", "wrong email")
   assert.isArray(validatedData.error)
 })
 
@@ -26,13 +18,20 @@ test('should return only one error if single incorrect data is passed', async ({
     first_name: "John",
     last_name: "Doe",
     username:"adgdg",
-    email: "wrong email",
+    email: "email",
+    password: "123456789"
   })
   assert.equal(validatedData.error.length, 1)
 })
 
-test('should return more than one error if multiple incorrect data is passed', async ({ assert }) => {
-  const validatedData = await userValidator("John", "Doe", "adgdg", "wrong email")
+test('should return more than one error if pass many incorrect data', async ({ assert }) => {
+  const validatedData = await userValidator({
+    first_name: "",
+    last_name: "Doe",
+    username:"username",
+    email: "wrong email",
+    password: "1234"
+  })
   assert.isAbove(validatedData.error.length, 1)
 })
 
@@ -42,7 +41,29 @@ test('should return undefined when pass correct data', async ({ assert }) => {
     last_name: "Doe",
     username:"adgdg",
     email: "john@mail.com",
+    password: "123456789"
   })
 
   assert.equal(validatedData.error, undefined)
+})
+
+test('should return only one error if pass incorrect password', async ({ assert }) => {
+  const validatedData = await userValidator({
+    first_name: "John",
+    last_name: "Doe",
+    username:"adgdg",
+    email: "john@mail.com",
+    password: "1"
+  })
+  assert.equal(validatedData.error.length, 1)
+})
+test('should return error if pass not unique username', async ({ assert }) => {
+  const validatedData = await userValidator({
+    first_name: "John",
+    last_name: "Doe",
+    username:"adgdg",
+    email: "john@mail.com",
+    password: "123454"
+  })
+  assert.isOk(validatedData.error)
 })
