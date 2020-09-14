548bfd968c0ddfaae2d7abab03c9029a457a64f2
panuwat-moonkham <panuwat.moonkham00@gmail.com>
2020-09-14 18:48:04 +0700

    edit post util

--- util/postUtil.js
+++ util/postUtil.js
@@ -30,6 +30,35 @@ class PostUtil {
         .then(response => response.first())
     }
 
+    async deletById(postInstance){
+        const { id } = postInstance.params
+        const posts = await this._Post.find(id)
+
+        if(!posts){
+            return {status : 500 ,error : `Not Found ${id}` , data : undefined};
+        }
+        posts.delete()
+        await posts.save();
+
+        return {status : 200 ,error : undefined , data : 'complete'};
+    }
+
+    async updateById(postInstance,references){
+        const { id } = postInstance.params
+        let posts = await this._Post.find(id)
+
+        if(!posts){
+            return {status : 500 ,error : `Not Found ${id}` , data : undefined};
+        }
+
+        posts.merge(postInstance.body)
+        await posts.save();
+    
+        posts = this._Post.query().where({post_id : id})
+        
+        return this._withReferences(categories,references).fetch().then(response => response.first())
+    }
+
     _withReferrnces(instance,references){
         if(references){
             const extractedReferences = references.split(",")
