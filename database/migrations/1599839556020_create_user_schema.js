f42f28add17ca0c53182da4cf94c0d39e32761e8
panuwat-moonkham <panuwat.moonkham00@gmail.com>
2020-09-15 06:33:32 +0700

    edit user migration

--- database/migrations/1599839556020_create_user_schema.js
+++ database/migrations/1599839556020_create_user_schema.js
@@ -11,7 +11,8 @@ class CreateUserSchema extends Schema {
       table.string('last_name', 120).notNullable()
       table.string('username', 120).notNullable().unique()
       table.string('email', 120).notNullable().unique()
-      table.string('password', 15).notNullable()
+      table.string('password', 100).notNullable()
+      table.string('status', 50).defaultTo('user')
       table.timestamps()
     })
   }
