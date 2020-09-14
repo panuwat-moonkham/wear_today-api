69e0134102d2dba693e9b41212cc84b126184eca
devillkaow <devillkaow@gmail.com>
2020-09-15 03:27:47 +0700

    add test-post-util

---
+++ test/unit/post-util.spec.js
@@ -0,0 +1,15 @@
+const PostUtil = require("../../util/postUtil")
+const Post = use('App/Models/Post')
+const { test } = use("Test/Suite")("Post Util")
+
+test("should get more than one when get many posts", async ({ assert }) => {
+    const postUtil = new PostUtil(Post)
+    const posts = await postUtil.getAll()
+    assert.isAbove(posts.rows.length, 1)
+})
+
+test("should return object created from PostUtil", async ({assert}) => {
+    const post =  new PostUtil(Post)
+    const {post_id} = await post.getById(1)
+    assert.isOk(post)
+})
