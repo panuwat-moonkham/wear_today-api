aef66509ab18f122e714c9caa68704cf44a13065
devillkaow <devillkaow@gmail.com>
2020-09-15 03:01:27 +0700

    edit comment-schema

--- database/migrations/1599839596221_create_comment_schema.js
+++ database/migrations/1599839596221_create_comment_schema.js
@@ -7,10 +7,23 @@ class CreateCommentSchema extends Schema {
   up () {
     this.create('comments', (table) => {
       table.increments('comment_id')
-      table.string('comment_content')
+      table.string('comment_content').notNullable()
       table.timestamp('comment_date').default(this.fn.now())
+      table.integer('user_id').unsigned()
+      table.integer('post_id').unsigned()
       table.timestamps()
 
+      table
+      .foreign('user_id')
+      .references('users.user_id')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+
+      table
+      .foreign('post_id')
+      .references('posts.post_id')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
     })
   }
 
