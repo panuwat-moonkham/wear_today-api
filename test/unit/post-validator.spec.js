235e25cd001c1f7ad12e035d250006a4467104af
devillkaow <devillkaow@gmail.com>
2020-09-15 06:38:36 +0700

    edit test-post

--- test/unit/post-validator.spec.js
+++ test/unit/post-validator.spec.js
@@ -6,15 +6,15 @@ const postValidator = require('../../service/PostValidator')
 test('should return error if pass incorrect data', async ({assert}) => {
   const validateData = await postValidator({
     post_title: '',
-    user_id: '1'
+    description: 'ssss'
   })
   assert.isOk(validateData.error);
 })
 
 test('should return only one error if pass single incorrect data', async ({assert}) => {
     const validatedData = await postValidator({
-        post_title: '',
-        user_id: '1'
+        post_title: '90s style',
+        description: ''
     })
     assert.equal(validatedData.error.length, 1)
 })
@@ -22,7 +22,7 @@ test('should return only one error if pass single incorrect data', async ({asser
 test('should return undefined when pass correct data', async ({assert}) => {
     const validatedData = await postValidator({
         post_title: '90s style',
-        user_id: '1'
+        description: 'saaaaaaaaaaa'
     })
     assert.equal(validatedData.error, undefined)
 })
@@ -30,8 +30,15 @@ test('should return undefined when pass correct data', async ({assert}) => {
 test('should return object when pass correct data ', async ({assert}) => {
     const validatedData = await postValidator({
         post_title: '90s style' ,
-        user_id: '1'
+        description: 'saaaaaaaaaaa'
     })
     assert.isObject(validatedData);
 })
-  
+
+test('should return more than one error if pass many incorrect data', async ({ assert }) => {
+    const validatedData = await postValidator({
+        post_title: '' ,
+        description: ''
+    })
+    assert.isAbove(validatedData.error.length, 1)
+  })
